d12888f9d2f1411690442db2986a46030a87956e
Vladyslav Davydenko <vladyslav@hey.com>
2024-09-07 22:11:47 +0300

    Update device model

--- app/jobs/populate_stats_job.rb
+++ app/jobs/populate_stats_job.rb
@@ -2,10 +2,8 @@ class PopulateStatsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    error_keys = %w[pd.errCode bms_slave.errCode bms_bmsStatus.errCode bms_slave.allErrCode]
     Device.find_each do |device|
-      data = device.get_all_quota
-      Stat.create(data: data, error: data.values_at(*error_keys).any? { _1 != 0 }, device: device)
+      Stat.create(device: device, data: device.all_quota, error: device.has_errors?)
     end
   end
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,24 @@
 class Device < ApplicationRecord
   has_many :stats
 
+  def all_quota
+    @all_quota ||= RestClient.new.all_quota(self)
+  end
+
+  def refresh_quota
+    @all_quota = nil
+    all_quota
+  end
+
   def get_all_quota
     RestClient.new.all_quota(self)
+  end
+
+  def error_keys
+    %w[pd.errCode bms_slave.errCode bms_bmsStatus.errCode bms_slave.allErrCode]
+  end
+
+  def has_errors?
+    all_quota.values_at(*error_keys).any? { _1 != 0 }
   end
 end
